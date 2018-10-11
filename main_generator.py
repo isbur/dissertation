ccccf101edb02f6e41d80b04f498eaf47e2359a1
Igor B <isburenkov@gmail.com>
2018-10-11 05:22:37 +0300

    Операция сложения
    Обеспечена замкнутость: сумма двух классов с распределением - класс с распределением.

--- main_generator.py
+++ main_generator.py
@@ -1,4 +1,19 @@
-import setRespondentType
+from setRespondentType import RespondentType
 import weightedChoice 
 
-d = setRespondentType.setType("Казахи-студенты.xlsx")
+from operator import add
+from os import listdir
+from os.path import isfile, join
+
+
+N = 1
+
+RespondentTypes = {}
+onlyfiles = [f for f in listdir("./") if isfile(join("./", f))]
+for file in onlyfiles:
+    if file.find(".xlsx") != -1:
+        RespondentTypes[file[0:len(file)-5]] = RespondentType.setViaFile(file)
+
+
+#for i in range(N):
+    
--- setRespondentType.py
+++ setRespondentType.py
@@ -1,33 +1,50 @@
 from Equation import Expression
 from openpyxl import load_workbook
+from operator import add, mul
 import re 
 
 # filename = "Казахи-студенты.xlsx"
-def setType(my_filename):
-    wb = load_workbook(filename = my_filename)
-    ws = wb.active
+class RespondentType:
+
     
-    Distributions = []
-    for row in ws:
-        Distribution = []
-        for cell in row:
-            if cell.value == None:
-                continue
-            if str(cell.value).find('SUM') != -1:
-                continue
-            if re.search('[а-яА-ЯёЁ]', str(cell.value)):
-                continue
-            
-            if cell.value == "f:":
-                #print("Hello!")
-                i, j = cell.row, cell.col_idx
-                fn = Expression(str(ws.cell(row = i, column = j + 1).value), "x")
-                #print(fn)
-                Distribution = Distribution + [fn(x) for x in range(ws.cell(row = i, column = j + 2).value, ws.cell(row = i, column = j + 3).value)]
-                break
+    def __init__(self, Distributions):
+        self.Distributions = Distributions
+    
+    
+    @classmethod
+    def setViaFile(cls, my_filename):
+        wb = load_workbook(filename = my_filename)
+        ws = wb.active
+        
+        Distributions = []
+        for row in ws:
+            Distribution = []
+            for cell in row:
+                if cell.value == None:
+                    continue
+                if str(cell.value).find('SUM') != -1:
+                    continue
+                if re.search('[а-яА-ЯёЁ]', str(cell.value)):
+                    continue
                 
-            
-            Distribution.append(cell.value)
-        Distributions.append(Distribution)
+                if cell.value == "f:":
+                    #print("Hello!")
+                    i, j = cell.row, cell.col_idx
+                    fn = Expression(str(ws.cell(row = i, column = j + 1).value), "x")
+                    #print(fn)
+                    Distribution = Distribution + [fn(x) for x in range(ws.cell(row = i, column = j + 2).value, ws.cell(row = i, column = j + 3).value)]
+                    break
+                    
+                
+                Distribution.append(cell.value)
+            Distributions.append(Distribution)
+        return cls(Distributions)
     
-    return Distributions
+    
+    def __add__(self, other):
+        Sum = []
+        for i in range( len(self.Distributions) ):
+            list1 = self.Distributions[i]
+            list2 = other.Distributions[i]
+            Sum.append( list( map(add, list1, list2) ) )
+        return RespondentType(Sum)
