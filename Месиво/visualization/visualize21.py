9ea0eb8c045c495e2d7fe1d415926d695b710aa5
Igor B <isburenkov@gmail.com>
2018-11-19 00:13:50 +0300

    Починил 21 вопрос

--- Месиво/visualization/visualize21.py
+++ Месиво/visualization/visualize21.py
@@ -39,35 +39,7 @@ for paragraph in wd.paragraphs:
 
 count = 0
 for Label in Labels:
-    #if count == 3:
-    #    break
     count += 1
-    if (count > 0) and (count < 11):
-        
-        fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-        column = [cell.value for cell in [column for column in ws.iter_cols(min_col=count, max_col=count)][0]][1:]
-        hist = np.histogram(column, bins = range(0, len(Labels[count-1][1:])+1 ) )
-        wedges, texts = ax.pie( hist[0], labels=Labels[count-1][1:])     
-        ax.set_title(Labels[count-1][0])
-        plt.savefig("Вопрос "+str(count)+".png")
-        plt.close()
-        
-        
-    if count == 11:
-        
-        heights = []
-        for i in range(15):
-            column = [cell.value for cell in [column for column in ws.iter_cols(min_col=count+i, max_col=count+i)][0]][1:]
-            hist = np.histogram(column, bins = range(0, 3) ) 
-            heights.append(hist[0][0])
-        
-        fig, ax = plt.subplots()
-        barplot = plt.bar(x = [x for x in range(1, 16)], height=heights)
-        ax.set_title(Labels[count-1][0])
-        plt.savefig("Вопрос "+str(count)+".png")
-        plt.close()
-        
-        
     if count > 11:
         
         if count == 21:
@@ -79,12 +51,4 @@ for Label in Labels:
             plt.savefig("Вопрос "+str(count)+".png")
             plt.close()
             continue
-        
-        fig, ax = plt.subplots(figsize=(6, 3), subplot_kw=dict(aspect="equal"))
-        column = [cell.value for cell in [column for column in ws.iter_cols(min_col=count+14, max_col=count+14)][0]][1:]
-        hist = np.histogram(column, bins = range(0, len(Labels[count-1][1:])+1 ) )
-        wedges, texts = ax.pie( hist[0], labels=Labels[count-1][1:])     
-        ax.set_title(Labels[count-1][0])   
-        plt.savefig("Вопрос "+str(count)+".png")
-        plt.close()
        
--- Месиво/weightedChoice.py
+++ Месиво/weightedChoice.py
@@ -1,57 +1,28 @@
 import math
 import random
 
+# Maybe I should merge two loops into one
 def weightedChoice(weights, qnumber=0):
     totals = []
     running_total = 0
-    #if qnumber == 34:
-        #print(weights)
     for w in weights:
-        #if qnumber == 34:
-            #print(w, type(w))
         if ( type(w)==type(1.0) ) or ( type(w)==type(1) ):
-            #if qnumber == 34:
-                #print("Hello")
             running_total += w
             totals.append(running_total)
     
     rnd = random.random() * running_total
-    #if qnumber == 34:
-        #print(rnd, totals)
     for i, total in enumerate(totals):
-        #if qnumber == 34:
-            #print(rnd, total, i) 
         if rnd < total:
-            if qnumber == 34:
-                return i + 20
             return i
-
-#Distributions = [
-    ##1
-    ## https://ru.wikipedia.org/wiki/%D0%9D%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5_%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D1%8B
-    #{'0':[35595, 106466, 9393]},
-    ##2
-    #{'0':[1,0,0],
-     #'1':[0,1,0], 
-     #'2':[0,0,1]
-     #},
-    ##3
-    #{
-        #'0':[80,16]+([4-x for x in range(0,4)]), 
-        #'1':[50 - x for x in range(0, 50)],
-        #'2':[math.exp(-x**2) for x in range(0,5)]
-             #}
-#]
+        
 
 def generateAnswer(myRespondentTypeObject):
     Answer = []
     for i, Distribution in enumerate(myRespondentTypeObject.Distributions):
-        #print(i)
-        #if i ==34:
-            #print(i, ": ", Distribution)
         Answer.append(weightedChoice(Distribution, i))
-    #print(Answer)
-    
+        if i == 34:
+            Answer[len(Answer)-1] += 20
+    print(Answer)
     return Answer
     
     
