a683fbcf133fb34e04c52f8f0647aced09581471
Igor <isburenkov@gmail.com>
2019-08-01 13:24:00 +0000

    Trying to get correct corr matrix

--- corr.r
+++ corr.r
@@ -142,34 +142,34 @@ for (i in c(1:nRows)){
 # -1 for each other excluding dummies ####
 ##########################################
 pureData11 = pureData10
-D = pureData11
-for (i in c(1:nRows)){
-  for (j in c(1:nCols)) {
-    item = D[i,j]
-    
-    item.x = rownames(D)[i]
-    if (grepl("_", item.x, fixed = TRUE)) {
-        item.x.first = strsplit(item.x, split = "_")[[1]][1]
-        item.x.second = strsplit(item.x, split = "_")[[1]][2]
-    } else {
-      next
-    }
-    item.y = colnames(D)[j]
-    if (grepl("_", item.y, fixed = TRUE)) {
-      item.y.first = strsplit(item.y, split = "_")[[1]][1]
-      item.y.second = strsplit(item.y, split = "_")[[1]][2]
-    } else {
-      next
-    }
-    
-    if (item.x.first == item.y.first) {
-        if (i != j) {
-          pureData11[i,j] = -1
-        }
-    }
-    
-  }
-}
+# D = pureData11
+# for (i in c(1:nRows)){
+#   for (j in c(1:nCols)) {
+#     item = D[i,j]
+#     
+#     item.x = rownames(D)[i]
+#     if (grepl("_", item.x, fixed = TRUE)) {
+#         item.x.first = strsplit(item.x, split = "_")[[1]][1]
+#         item.x.second = strsplit(item.x, split = "_")[[1]][2]
+#     } else {
+#       next
+#     }
+#     item.y = colnames(D)[j]
+#     if (grepl("_", item.y, fixed = TRUE)) {
+#       item.y.first = strsplit(item.y, split = "_")[[1]][1]
+#       item.y.second = strsplit(item.y, split = "_")[[1]][2]
+#     } else {
+#       next
+#     }
+#     
+#     if (item.x.first == item.y.first) {
+#         if (i != j) {
+#           pureData11[i,j] = -1
+#         }
+#     }
+#     
+#   }
+# }
 
 
 #####################################
@@ -200,6 +200,7 @@ incorporate = function(n, B, D) {
 }
 B = incorporate(n, B, D)
 
+
 # Third, approximate it with near PD matrix
 # install.packages("Matrix")
 library(Matrix)
@@ -212,17 +213,43 @@ positify = function(B){
     return(D)
 }
 
+library(matrixcalc)
 # Some mad attempts
-i = 0
+
 m = 100
+B = unname(B)
 B = incorporate(n, B, D)
-myDet = -1
-while (myDet < 0 || i < 100) {
-    B = positify(B)
-    B = incorporate(n, B, D)
-    myDet = det(B)
-    i = i + 1
+isPD = FALSE
+#i = 0
+#iterations_limit = 1000000
+#while (isPD || i < iterations_limit) {
+#    B = positify(B)
+#    B = incorporate(n, B, D)
+#    B = round(B, digits = 4)
+#    print(det(B))
+#    isPD = is.positive.definite(B)
+#    i = i + 1
+#}
+
+# Complexify
+initial_conditions_number = 0
+while(!isPD) {
+    B = rcorrmatrix(n)
+    i = 0
+    iterations_limit = 30
+    while (i < iterations_limit) {
+        B = positify(B)
+        B = incorporate(n, B, D)
+        B = round(B, digits = 4)
+        print(det(B))
+        isPD = is.positive.definite(B)
+        i = i + 1
+    }
 }
+
+#if (i == iterations_limit) {
+#    print("WARNING!!!")
+#}
 
 pureData12 = B
 
--- genData.r
+++ genData.r
@@ -11,13 +11,7 @@ for (i in c(1:n_variables)) {
     def = defData(def, varname = names[i], dist = corrected_dists[i], formula = formulas[i])  # Alredy don't remember how "names" was got
 }
 
-Sigma = read.csv("corr.csv", header = TRUE)
+Sigma = read.csv(file = "corr.csv", header = TRUE, row.names = 1)
 Sigma = as.matrix(Sigma)
+Sigma = unname(Sigma) # otherwise isSymmetric may be FALSE
 dt = genCorFlex(n_individuals, def, corMatrix = Sigma)
-
-
-
-
-
-
-
